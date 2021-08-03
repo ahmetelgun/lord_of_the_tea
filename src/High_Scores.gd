4eabde1f65cef80fc46afaa6689f31f443058289
Ahmet Elgun <ahmetelgn@gmail.com>
2021-08-03 18:10:05 +0300

    add scroll to high scores

--- src/High_Scores.gd
+++ src/High_Scores.gd
@@ -1,12 +1,12 @@
 extends Node
 
 func _ready():
-	yield(SilentWolf.Scores.get_high_scores(10), "sw_scores_received")
+	yield(SilentWolf.Scores.get_high_scores(0), "sw_scores_received")
 	for i in SilentWolf.Scores.scores:
-		var new_score = get_node("ScoreContainer/1").duplicate()
+		var new_score = get_node("ScrollContainer/ScoreContainer/1").duplicate()
 		new_score.text = "{name} - {score}".format({"name": i.player_name, "score": str(int(i.score))})
 		new_score.visible = true
-		get_node("ScoreContainer").add_child(new_score)
+		get_node("ScrollContainer/ScoreContainer").add_child(new_score)
 
 
 func _on_Button_pressed():
